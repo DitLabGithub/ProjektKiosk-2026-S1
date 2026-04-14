7b17ba0232e6f6818f63f663514d75f9056f370d
Nixfil <nikolaifilipov.tn@gmail.com>
2026-04-14 15:56:29 +0200

    Sprites and Button
    Sprites imported;
    First Prototype of the dialogue button;

---
+++ Assets/Scripts/DialogueButton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TextMeshProEssentials;
+
+public class DialogueButton : MonoBehaviour
+{
+    public Unity.System.UI.Image icon;
+    public TextMeshProUGUI text;
+
+}
